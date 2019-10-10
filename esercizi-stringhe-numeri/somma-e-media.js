24299e0a6120b90c0f056d3c5d733e2996a93dd9
lucam1990 <54977754+lucam1990@users.noreply.github.com>
2019-10-10 15:21:06 +0200

    Test somma-e-media
    è giusto come scritto il codice? Grazie

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,15 @@
   
   http://www.imparareaprogrammare.it
 */
+
+
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+
+somma = (a+b+c+d+e);
+media = (somma/5);
+
+console.log('somma = '+somma + ', media = '+media);
